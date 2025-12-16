582d35396cb8306920a91082dce2c17f5e7e78ae
habi8 <bsse1422@iit.du.ac.bd>
2025-12-16 13:21:06 +0600

    Changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,25 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "EXPENSE",
+  title: "XPENSE",
   description: "A web based income and expense tracker",
   generator: "v0.app",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/XPENSE.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/XPENSE.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/XPENSE.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/XPENSE.png",
   },
 };
 
