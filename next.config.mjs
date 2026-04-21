0c215ba87c65b362f055f932785494333cfd6052
habi8 <bsse1422@iit.du.ac.bd>
2026-04-21 20:31:26 +0600

    tested locally

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL ?? process.env.SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
+      process.env.SUPABASE_PUBLISHABLE_KEY ??
+      process.env.SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL:
+      process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL ?? process.env.SUPABASE_REDIRECT_URL,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
 }
 
 export default nextConfig
